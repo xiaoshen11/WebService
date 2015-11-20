442f37c02803541ec74c40fe1b19c2962cd85856
347418287 <347418287@qq.com>
2015-11-20 21:24:23 +0800

    dyh

--- WebService/src/com/dyh/test/PhoneNumTest.java
+++ WebService/src/com/dyh/test/PhoneNumTest.java
@@ -1,5 +1,16 @@
 package com.dyh.test;
 
+import client.MobileCodeWS;
+import client.MobileCodeWSSoap;
+
 public class PhoneNumTest {
 
+public static void main(String[] args) {
+		
+		MobileCodeWS mc = new MobileCodeWS();
+		MobileCodeWSSoap soap = mc.getMobileCodeWSSoap();
+		String msg = soap.getMobileCodeInfo("18271483864", null);
+		System.out.println(msg);
+	}
+	
 }
